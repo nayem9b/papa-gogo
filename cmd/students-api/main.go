d395818b78d0806d0d51c3a46ef3777d2d670e5b
Md Razwan Niam <78756747+nayem9b@users.noreply.github.com>
2025-03-19 08:47:25 +0600

    server working

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,6 +1,24 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Worlddd!")
+
+	//setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	//start server
+	server := http.Server{
+		Addr: ":8008",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+	fmt.Println("Server is running on port 8080")
 }
