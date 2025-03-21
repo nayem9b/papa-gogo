291e2de9c014ead0ae1ad11b856c83a9ec0b5728
Md Razwan Niam <78756747+nayem9b@users.noreply.github.com>
2025-03-21 06:08:09 +0600

    added post method

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"hello/internal/http/handlers/student"
 	"net/http"
 )
 
@@ -9,10 +10,13 @@ func main() {
 
 	//setup router
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	// router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello, World!"))
+	// })
 
+	router.HandleFunc("GET /api/students",student.New())
+
+	router.HandleFunc("POST /api/students",student.New())
 	//start server
 	server := http.Server{
 		Addr: ":8008",
