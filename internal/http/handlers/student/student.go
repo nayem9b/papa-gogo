8936c94374b67ee48e660aac9eb25e7088b2185b
Md Razwan Niam <78756747+nayem9b@users.noreply.github.com>
2025-03-21 09:10:15 +0600

    added response method struct

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,10 +1,38 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"hello/internal/utils/response"
+	"io"
+
+	// "go/types" removed as it's not needed
+	"log/slog"
+	"net/http"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to the student API"))
+
+		type Student struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Email string `json:"email"`
+		}
+
+		var students Student
+		err := json.NewDecoder(r.Body).Decode(&students)
+		if errors.Is(err, io.EOF){
+			response.Writejson(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		// if err != nil {
+        //     http.Error(w, err.Error(), http.StatusBadRequest)
+        //     return
+        // }
+		slog.Info("Request received")
+		// w.Write([]byte("Welcome to the student API"))
+		response.Writejson(w, http.StatusOK, students)
 	}
 
 }
